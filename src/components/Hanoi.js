7a0d120fd62e31ea2dfc2d904c1d6f49fb426786
Aryan Chavan <aryan@aryansdomain.com>
2022-12-07 21:44:26 +0530

    add util functions`

--- src/components/Hanoi.js
+++ src/components/Hanoi.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
 
 function Hanoi() {
-  return (
-    <div>Hanoi</div>
-  )
+	return <div className="hanoi">Hanoi</div>;
 }
 
-export default Hanoi
+function getInitialState(n) {
+	return [
+		Array(n)
+			.fill(0)
+			.map((val, index) => index)
+			.reverse(),
+		[],
+		[],
+	];
+}
+
+function checkWin(state) {
+	if (!state[0].length && !state[1].length) {
+		// first two towers are empty
+		return true;
+	}
+
+	return false;
+}
+
+export default Hanoi;
